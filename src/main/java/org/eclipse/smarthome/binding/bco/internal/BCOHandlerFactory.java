c205de340a8b6d89076fb01b3b64b4cd9cafb3ed
Divine Threepwood <divine@openbase.org>
2021-01-14 19:34:35 +0100

    update licence header to 2021

--- src/main/java/org/eclipse/smarthome/binding/bco/internal/BCOHandlerFactory.java
+++ src/main/java/org/eclipse/smarthome/binding/bco/internal/BCOHandlerFactory.java
@@ -4,7 +4,7 @@ package org.eclipse.smarthome.binding.bco.internal;
  * #%L
  * BCO Binding
  * %%
- * Copyright (C) 2018 - 2020 openbase.org
+ * Copyright (C) 2018 - 2021 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
